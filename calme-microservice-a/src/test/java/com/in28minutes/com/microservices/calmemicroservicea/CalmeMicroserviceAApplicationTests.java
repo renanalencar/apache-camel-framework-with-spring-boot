f789231cabb8f192cf7628a31bc41506d304d62c
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-06-09 08:55:53 -0300

    Creating your first Apache Camel Route

---
+++ calme-microservice-a/src/test/java/com/in28minutes/com/microservices/calmemicroservicea/CalmeMicroserviceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.com.microservices.calmemicroservicea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmeMicroserviceAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ calme-microservice-b/src/test/java/com/in28minutes/com/microservices/calmemicroserviceb/CalmeMicroserviceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.com.microservices.calmemicroserviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmeMicroserviceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
