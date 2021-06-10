3ca31dd720cab886884a96e1ba20835ea54b4b79
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-06-10 09:41:13 -0300

    Transformation and Processing after Unmarshalling in Camel Route

--- camel-microservice-b/src/main/java/com/in28minutes/com/microservices/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
+++ camel-microservice-b/src/main/java/com/in28minutes/com/microservices/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
@@ -3,8 +3,13 @@
  */
 package com.in28minutes.com.microservices.camelmicroserviceb.routes;
 
+import java.math.BigDecimal;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.in28minutes.com.microservices.camelmicroserviceb.CurrencyExchange;
@@ -15,6 +20,11 @@ import com.in28minutes.com.microservices.camelmicroserviceb.CurrencyExchange;
  */
 @Component
 public class ActiveMqReceiverRouter extends RouteBuilder {
+	
+	
+	
+	@Autowired
+	private MyCurrencyExchangeProcessor myCurrencyExchangeProcessor;
 
 	@Override
 	public void configure() throws Exception {
@@ -24,8 +34,35 @@ public class ActiveMqReceiverRouter extends RouteBuilder {
 		
 		from("activemq:my-activemq-queue")
 		.unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+		.bean(myCurrencyExchangeProcessor)
 		.to("log:received-message-from-active-qm");
 	}
 
 }
 
+@Component
+class MyCurrencyExchangeProcessor {
+	
+	Logger logger = LoggerFactory.getLogger(MyCurrencyExchangeProcessor.class);
+	
+	public void processMessage(CurrencyExchange currencyExchange) {
+		
+		logger.info("Do some processing with currencyExchange.getConversionMultiple value wich is {}", 
+				currencyExchange.getConversionMultiple());
+	}
+}
+
+@Component
+class MyCurrencyExchangeTransformer {
+	
+	Logger logger = LoggerFactory.getLogger(MyCurrencyExchangeProcessor.class);
+	
+	public CurrencyExchange processMessage(CurrencyExchange currencyExchange) {
+		
+		currencyExchange.setConversionMultiple(
+				currencyExchange.getConversionMultiple().multiply(BigDecimal.TEN));
+		
+		return currencyExchange;
+		
+	}
+}
