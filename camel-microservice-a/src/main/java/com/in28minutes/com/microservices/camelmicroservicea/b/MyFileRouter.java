503a5fc3382199a0c5666d08d818aa68c55c6392
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-06-10 17:13:58 -0300

    Creating Complex Deciders for Choice in Camel Route

--- camel-microservice-a/src/main/java/com/in28minutes/com/microservices/camelmicroservicea/b/MyFileRouter.java
+++ camel-microservice-a/src/main/java/com/in28minutes/com/microservices/camelmicroservicea/b/MyFileRouter.java
@@ -3,7 +3,14 @@
  */
 package com.in28minutes.com.microservices.camelmicroservicea.b;
 
+import java.util.Map;
+
+import org.apache.camel.ExchangeProperties;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,6 +20,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MyFileRouter extends RouteBuilder {
+	
+	@Autowired
+	private DeciderBean deciderBean;
 
 	@Override
 	public void configure() throws Exception {
@@ -22,12 +32,13 @@ public class MyFileRouter extends RouteBuilder {
 		.choice()
 			.when(simple("${file:ext} == 'xml'"))
 				.log("XML FILE")
-			.when(simple("${body} contains 'USD'"))
+			//.when(simple("${body} contains 'USD'"))
+			.when(method(deciderBean))	
 				.log("NOT XML FILE BUT CONTAINS USD")
 			.otherwise()
 				.log("NOT XML FILE")
 		.end()
-		.to("direct://log-file-values")
+//		.to("direct://log-file-values")
 		.to("file:files/output");
 		
 		from("direct:log-file-values")
@@ -39,4 +50,15 @@ public class MyFileRouter extends RouteBuilder {
 		
 	}
 	
+}
+
+@Component
+class DeciderBean {
+	
+	Logger logger = LoggerFactory.getLogger(DeciderBean.class);
+	
+	public boolean isThisConditionMet(String body, @Headers Map<String, String> headers, @ExchangeProperties Map<String, String> properties) {
+		logger.info("{}", body, headers, properties);
+		return true;
+	}
 }
