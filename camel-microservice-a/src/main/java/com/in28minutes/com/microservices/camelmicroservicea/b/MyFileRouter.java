de733ac515a6cffda742ccf14954ad48dd6e5074
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-06-10 17:03:51 -0300

    Creating Reusable Endpoints in Camel Routes

--- camel-microservice-a/src/main/java/com/in28minutes/com/microservices/camelmicroservicea/b/MyFileRouter.java
+++ camel-microservice-a/src/main/java/com/in28minutes/com/microservices/camelmicroservicea/b/MyFileRouter.java
@@ -20,15 +20,22 @@ public class MyFileRouter extends RouteBuilder {
 		.routeId("Files-Input-Route")
 		.transform().body(String.class)
 		.choice()
-			.when(simple("${file:ext} ends with 'xml'"))
+			.when(simple("${file:ext} == 'xml'"))
 				.log("XML FILE")
 			.when(simple("${body} contains 'USD'"))
 				.log("NOT XML FILE BUT CONTAINS USD")
 			.otherwise()
 				.log("NOT XML FILE")
 		.end()
-		.log("${messageHistory} ${headers.CamelFileAbsolute}")
+		.to("direct://log-file-values")
 		.to("file:files/output");
+		
+		from("direct:log-file-values")
+		.log("${messageHistory} ${file:absolute.path}")
+		.log("${file:name} ${file:name.ext} ${file:name.noext} ${file:onlyname}")
+		.log("${file:onlyname.noext} ${file:parent} ${file:path} ${file:absolute}")
+		.log("${file:size} ${file:modified}")
+		.log("${routeId} ${camelId} ${body}");
 		
 	}
 	
