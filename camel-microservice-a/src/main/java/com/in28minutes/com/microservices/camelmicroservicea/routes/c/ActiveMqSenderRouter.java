6cd262027fbfd4face8175c94f14f492c9fcaea6
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-06-10 09:26:11 -0300

    Unmarshalling JSON Message to Java Bean in ActiveMQ Camel Route

--- camel-microservice-a/src/main/java/com/in28minutes/com/microservices/camelmicroservicea/routes/c/ActiveMqSenderRouter.java
+++ camel-microservice-a/src/main/java/com/in28minutes/com/microservices/camelmicroservicea/routes/c/ActiveMqSenderRouter.java
@@ -9,11 +9,15 @@ public class ActiveMqSenderRouter extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		// timer
-		from("timer:activemq-timer?period=10000")
-		.transform().constant("My message for Active MQ")
+//		from("timer:activemq-timer?period=10000")
+//		.transform().constant("My message for Active MQ")
+//		.log("${body}")
+//		.to("activemq:my-activemq-queue");
+		// queue
+		
+		from("file:files/json")
 		.log("${body}")
 		.to("activemq:my-activemq-queue");
-		// queue
 		
 	}
 
--- camel-microservice-b/src/main/java/com/in28minutes/com/microservices/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
+++ camel-microservice-b/src/main/java/com/in28minutes/com/microservices/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
@@ -4,7 +4,10 @@
 package com.in28minutes.com.microservices.camelmicroserviceb.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
+
+import com.in28minutes.com.microservices.camelmicroserviceb.CurrencyExchange;
 
 /**
  * @author renan
@@ -15,8 +18,14 @@ public class ActiveMqReceiverRouter extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
+		//JSON
+		//CurrencyExchange
+		//{ "id": 1000, "from": "USD", "to": "INR", "conversionMultiple":70 }
+		
 		from("activemq:my-activemq-queue")
+		.unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
 		.to("log:received-message-from-active-qm");
 	}
 
 }
+
