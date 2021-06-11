16cf97859800f18246609f0dcbe6a32920b691c7
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-06-11 16:50:08 -0300

    Exploring Aggregation Enterprise Integration Pattern in Camel

--- camel-microservice-a/src/main/java/com/in28minutes/com/microservices/camelmicroservicea/patterns/EipPatternsRouter.java
+++ camel-microservice-a/src/main/java/com/in28minutes/com/microservices/camelmicroservicea/patterns/EipPatternsRouter.java
@@ -1,17 +1,52 @@
 package com.in28minutes.com.microservices.camelmicroservicea.patterns;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.in28minutes.com.microservices.camelmicroservicea.CurrencyExchange;
 
 @Component
 public class EipPatternsRouter extends RouteBuilder {
 	
 	@Autowired
-	private SplitterComponent splitter;
+		private SplitterComponent splitter;
+
+	/**
+	 * @author renan
+	 *
+	 */
+	public class ArrayListAggregationStrategy implements AggregationStrategy {
+
+		//1,2,3
+		//null, 1 => [1]
+		//1, 2 => [1,2]
+		//[1,2], 3 => [1,2,3]
+		
+		@Override
+		public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+			Object newBody = newExchange.getIn().getBody();
+			ArrayList<Object> list = null;
+			if (oldExchange == null) {
+				list = new ArrayList<Object>();
+				list.add(newBody);
+				newExchange.getIn().setBody(list);
+				return newExchange;
+			} else {
+				list = oldExchange.getIn().getBody(ArrayList.class);
+				list.add(newBody);
+				return oldExchange;
+			}
+		}
+
+	}
 
 	@Override
 	public void configure() throws Exception {
@@ -30,11 +65,22 @@ public class EipPatternsRouter extends RouteBuilder {
 		
 		
 		//Message, Message2, Message3
-		from("file:files/csv")
-		.convertBodyTo(String.class)
-//		.split(body(), ",")
-		.split(method(splitter))
-		.to("activemq:slipt-queue");
+//		from("file:files/csv")
+//		.convertBodyTo(String.class)
+////		.split(body(), ",")
+//		.split(method(splitter))
+//		.to("activemq:slipt-queue");
+		
+		//Aggregate
+		//Messages => Aggregate => Endpoint
+		//to , 3
+		from("file:files/aggregate-json")
+		.unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+		.aggregate(simple("${body.to}"), new ArrayListAggregationStrategy())
+		.completionSize(3)
+		.completionTimeout(HIGHEST)
+		.to("log:aggregate-json");
+		
 	}
 
 }
