1226be160eabc0e661a263b70542482ccb280745
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-06-11 16:00:10 -0300

    Exploring Splitter Enterprise Integration Pattern in Camel

--- camel-microservice-a/src/main/java/com/in28minutes/com/microservices/camelmicroservicea/patterns/EipPatternsRouter.java
+++ camel-microservice-a/src/main/java/com/in28minutes/com/microservices/camelmicroservicea/patterns/EipPatternsRouter.java
@@ -1,10 +1,17 @@
 package com.in28minutes.com.microservices.camelmicroservicea.patterns;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EipPatternsRouter extends RouteBuilder {
+	
+	@Autowired
+	private SplitterComponent splitter;
 
 	@Override
 	public void configure() throws Exception {
@@ -16,10 +23,26 @@ public class EipPatternsRouter extends RouteBuilder {
 //		.multicast()
 //		.to("log:something1", "log:something2", "log:something3");
 		
+//		from("file:files/csv")
+//		.unmarshal().csv()
+//		.split(body())
+//		.to("activemq:slipt-queue");
+		
+		
+		//Message, Message2, Message3
 		from("file:files/csv")
-		.unmarshal().csv()
-		.split(body())
+		.convertBodyTo(String.class)
+//		.split(body(), ",")
+		.split(method(splitter))
 		.to("activemq:slipt-queue");
 	}
 
+}
+
+@Component
+class SplitterComponent {
+	public List<String> splitInput(String body) {
+//		return List.of("ABC", "DEF", "GHI"); // List.of() faz parte do Java 9
+		return Arrays.asList("ABC", "DEF", "GHI");
+	}
 }
